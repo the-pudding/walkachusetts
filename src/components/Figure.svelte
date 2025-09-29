f390fc5b3ad67f0c2efedd5c71cb13409aad3549
Russell Samora <russellsamora@gmail.com>
2025-09-29 16:05:57 -0400

    adjustments

--- src/components/Figure.svelte
+++ src/components/Figure.svelte
@@ -1,10 +1,17 @@
 <script>
-	const { src, alt, caption } = $props();
+	const { src, alt, caption, top, bottom } = $props();
 	const video = src.endsWith(".mp4");
 	const illo = src.endsWith(".png");
+
+	const styles = `${top ? `margin-top: ${top};` : ""} ${
+		bottom ? `margin-bottom: ${bottom};` : ""
+	}`;
 </script>
 
-<figure class:video class:illo>
+<figure class:video class:illo style={styles}>
+	<!-- {#if caption}
+		<figcaption>{@html caption}</figcaption>
+	{/if} -->
 	{#if video}
 		<video autoplay loop muted playsinline>
 			<source {src} type="video/mp4" />
@@ -12,8 +19,8 @@
 	{:else}
 		<img {src} {alt} />
 	{/if}
-	{#if caption}
-		<figcaption>{@html caption}</figcaption>
+	{#if !illo}
+		<img class="frame" src="assets/illos/frame.png" alt="" aria-hidden="true" />
 	{/if}
 </figure>
 
@@ -23,13 +30,17 @@
 		width: 100%;
 		max-width: 360px;
 		margin: 2em auto;
+		position: relative;
+		padding: 1em;
 	}
 
-	figcaption {
-		display: none;
-		font-size: var(--14px);
-		text-align: center;
-		margin-top: 8px;
-		line-height: 1.2;
+	img.frame {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		pointer-events: none;
+		user-select: none;
 	}
 </style>
