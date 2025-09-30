4d48ed7cf2b65e4713482622b60a5c95602e81fd
Russell Samora <russellsamora@gmail.com>
2025-09-30 11:19:16 -0400

    update github pages

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -19,6 +19,10 @@
 
 <div class="c">
 	<div class="linear">
+		<div class="hero">
+			<h1>{copy.meta.title}</h1>
+			<p>{copy.meta.description}</p>
+		</div>
 		<CMS {body} {components} />
 	</div>
 	<div class="sidebar"><Sidebar {figures} {components} /></div>
@@ -34,5 +38,25 @@
 		margin: 0 auto;
 		padding: 0 1rem;
 		display: flex;
+	}
+
+	h1 {
+		display: inline-flex;
+		line-height: 1;
+		font-size: var(--18px);
+		background: var(--color-fg);
+		color: var(--color-bg);
+		padding: 0.5em;
+		text-transform: uppercase;
+		font-family: var(--sans);
+		margin: 0;
+		margin-top: 4rem;
+	}
+
+	.hero p {
+		font-family: var(--sans);
+		font-weight: bold;
+		margin: 0;
+		font-size: var(--28px);
 	}
 </style>
